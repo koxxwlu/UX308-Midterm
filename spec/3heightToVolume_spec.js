00a05c850615ec7404cb958e50ed3619518ee59a
koxxwlu <144965789+koxxwlu@users.noreply.github.com>
2025-03-07 00:38:18 +0000

    finish Q3

--- spec/3heightToVolume_spec.js
+++ spec/3heightToVolume_spec.js
@@ -1,8 +1,22 @@
 import { heightToVolume } from '../3heightToVolume.js'
 
 describe("A suite", function () {
-  it("testing 1", function () {
+  it("testing 1m", function () {
     let value = heightToVolume(1);
-    expect(value).toBe(1);
+    expect(value).toBe("1m (sqr)");
+  });
+});
+
+describe("A suite", function () {
+  it("testing 3m", function () {
+    let value = heightToVolume(3);
+    expect(value).toBe("27m (sqr)");
+  });
+});
+
+describe("A suite", function () {
+  it("testing 64m", function () {
+    let value = heightToVolume(64);
+    expect(value).toBe("262144m (sqr)");
   });
 });
